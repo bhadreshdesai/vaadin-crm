f069e8fb84eb2920d3f4146779d24bccfdd3f69c
Bhadresh Desai <bdesai@avoka.com>
2020-03-28 07:05:58 +0000

    Chapter-9 Defining the form component API and binding the data

--- src/main/java/com/vaadin/tutorial/crm/ui/ContactForm.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/ContactForm.java
@@ -1,5 +1,7 @@
 package com.vaadin.tutorial.crm.ui;
 
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
@@ -8,8 +10,13 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.shared.Registration;
 import com.vaadin.tutorial.crm.backend.entity.Company;
 import com.vaadin.tutorial.crm.backend.entity.Contact;
+
+import java.util.List;
 
 public class ContactForm extends FormLayout {
 
@@ -23,8 +30,15 @@ public class ContactForm extends FormLayout {
     Button delete = new Button("Delete");
     Button close = new Button("Cancel");
 
-    public ContactForm() {
+    Binder<Contact> binder = new BeanValidationBinder<>(Contact.class);
+
+    public ContactForm(List<Company> companies) {
         addClassName("contact-form");
+        binder.bindInstanceFields(this);
+
+        company.setItems(companies);
+        company.setItemLabelGenerator(Company::getName);
+        status.setItems(Contact.Status.values());
         add(firstName,
                 lastName,
                 email,
@@ -41,6 +55,57 @@ public class ContactForm extends FormLayout {
         save.addClickShortcut(Key.ENTER);
         close.addClickShortcut(Key.ESCAPE);
 
+        save.addClickListener(event -> validateAndSave());
+        delete.addClickListener(event -> fireEvent(new DeleteEvent(this, binder.getBean())));
+        close.addClickListener(event -> fireEvent(new CloseEvent(this)));
+        binder.addStatusChangeListener(event -> save.setEnabled(binder.isValid()));
+
         return new HorizontalLayout(save, delete, close);
+    }
+
+    private void validateAndSave() {
+        if (binder.isValid()) {
+            fireEvent(new SaveEvent(this, binder.getBean()));
+        }
+    }
+
+    public void setContact(Contact contact) {
+        binder.setBean(contact);
+    }
+
+    public <T extends ComponentEvent<?>> Registration addListener(Class<T> eventType, ComponentEventListener<T> listener) {
+        return getEventBus().addListener(eventType, listener);
+    }
+
+    // Events
+    public static abstract class ContactFormEvent extends ComponentEvent<ContactForm> {
+        private Contact contact;
+
+        protected ContactFormEvent(ContactForm source, Contact contact) {
+            super(source, false);
+            this.contact = contact;
+        }
+
+        public Contact getContact() {
+            return contact;
+        }
+    }
+
+    public static class SaveEvent extends ContactFormEvent {
+        SaveEvent(ContactForm source, Contact contact) {
+            super(source, contact);
+        }
+    }
+
+    public static class DeleteEvent extends ContactFormEvent {
+        DeleteEvent(ContactForm source, Contact contact) {
+            super(source, contact);
+        }
+    }
+
+    public static class CloseEvent extends ContactFormEvent {
+        CloseEvent(ContactForm source) {
+            super(source, null);
+        }
     }
 }
--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -9,6 +9,7 @@ import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import com.vaadin.tutorial.crm.backend.entity.Company;
 import com.vaadin.tutorial.crm.backend.entity.Contact;
+import com.vaadin.tutorial.crm.backend.service.CompanyService;
 import com.vaadin.tutorial.crm.backend.service.ContactService;
 
 @Route("")
@@ -21,14 +22,14 @@ public class MainView extends VerticalLayout {
     private Grid<Contact> grid = new Grid<>(Contact.class);
     private ContactForm form;
 
-    public MainView(ContactService contactService) {
+    public MainView(ContactService contactService, CompanyService companyService) {
         this.contactService = contactService;
         addClassName("list-view");
         setSizeFull();
         configureFilter();
         configureGrid();
 
-        form = new ContactForm();
+        form = new ContactForm(companyService.findAll());
         Div content = new Div(grid, form);
         content.addClassName("content");
         content.setSizeFull();
