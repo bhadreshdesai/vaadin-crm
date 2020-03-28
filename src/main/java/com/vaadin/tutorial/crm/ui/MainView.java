64244a1f4f6e992bcded60a75ead30f244d5c5b3
Bhadresh Desai <bdesai@avoka.com>
2020-03-28 08:11:58 +0000

    Chapter-10 Communicating between components using events and properties

--- src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
+++ src/main/java/com/vaadin/tutorial/crm/ui/MainView.java
@@ -1,8 +1,10 @@
 package com.vaadin.tutorial.crm.ui;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
@@ -26,23 +28,37 @@ public class MainView extends VerticalLayout {
         this.contactService = contactService;
         addClassName("list-view");
         setSizeFull();
-        configureFilter();
         configureGrid();
+        configureForm(companyService);
 
-        form = new ContactForm(companyService.findAll());
         Div content = new Div(grid, form);
         content.addClassName("content");
         content.setSizeFull();
 
-        add(filterText, content);
+        add(getToolbar(), content);
         updateList();
+        closeEditor();
     }
 
-    private void configureFilter() {
-        filterText.setPlaceholder("Filter by name...");
-        filterText.setClearButtonVisible(true);
-        filterText.setValueChangeMode(ValueChangeMode.LAZY);
-        filterText.addValueChangeListener(e -> updateList());
+    void addContact() {
+        grid.asSingleSelect().clear();
+        editContact(new Contact());
+        // Disable the grid untill the user clicks Cancel or Save where closeEditor is closed and the selection mode is set to Grid.SelectionMode.SINGLE
+        grid.setSelectionMode(Grid.SelectionMode.NONE);
+    }
+
+    private void closeEditor() {
+        form.setContact(null);
+        form.setVisible(false);
+        removeClassName("editing");
+        grid.setSelectionMode(Grid.SelectionMode.SINGLE);
+    }
+
+    private void configureForm(CompanyService companyService) {
+        form = new ContactForm(companyService.findAll());
+        form.addListener(ContactForm.SaveEvent.class, this::saveContact);
+        form.addListener(ContactForm.DeleteEvent.class, this::deleteContact);
+        form.addListener(ContactForm.CloseEvent.class, e -> closeEditor());
     }
 
     private void configureGrid() {
@@ -55,6 +71,44 @@ public class MainView extends VerticalLayout {
             return company == null ? "-" : company.getName();
         }).setHeader("Company");
         grid.getColumns().forEach(col -> col.setAutoWidth(true));
+        grid.asSingleSelect().addValueChangeListener(event -> editContact(event.getValue()));
+    }
+
+    private void deleteContact(ContactForm.DeleteEvent event) {
+        contactService.delete(event.getContact());
+        updateList();
+        closeEditor();
+    }
+
+    private void editContact(Contact contact) {
+        if (contact == null) {
+            closeEditor();
+        } else {
+            form.setContact(contact);
+            form.setVisible(true);
+            addClassName("editing");
+        }
+    }
+
+    private HorizontalLayout getToolbar() {
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+        filterText.addValueChangeListener(e -> updateList());
+
+        Button addContactButton = new Button("Add contact");
+        addContactButton.addClickListener(click -> addContact());
+
+        HorizontalLayout toolbar = new HorizontalLayout(filterText, addContactButton);
+        toolbar.addClassName("toolbar");
+        return toolbar;
+    }
+
+    private void saveContact(ContactForm.SaveEvent event) {
+        contactService.save(event.getContact());
+        // Is there a better way of updating single row in the grid?
+        updateList();
+        closeEditor();
     }
 
     private void updateList() {
